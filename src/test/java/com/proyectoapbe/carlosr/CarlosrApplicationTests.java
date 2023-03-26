40bf91830522f70212d520a3f8e84037414355da
prodalkimia <produccionesalkimiavzla@gmail.com>
2023-03-26 17:33:41 -0300

    Iniciando Springboot

---
+++ src/test/java/com/proyectoapbe/carlosr/CarlosrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoapbe.carlosr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarlosrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
