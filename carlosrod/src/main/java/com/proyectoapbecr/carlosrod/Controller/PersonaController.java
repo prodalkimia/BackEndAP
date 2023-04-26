66dde4d8af323c4f628a323e6cb8db1afc9fab85
prodalkimia <produccionesalkimiavzla@gmail.com>
2023-04-26 19:45:22 -0300

    Continuación Back y router

--- carlosrod/src/main/java/com/proyectoapbecr/carlosrod/Controller/PersonaController.java
+++ carlosrod/src/main/java/com/proyectoapbecr/carlosrod/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.proyectoapbecr.carlosrod.Entity.Persona;
 import com.proyectoapbecr.carlosrod.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -51,4 +53,8 @@ public class PersonaController {
         return persona;
     }
     
+    @GetMapping("personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
+    }
 }
